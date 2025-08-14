13e676d61734a2f7fb1b2d92a8021a12935cd1b2
Aegdae <jonnathasg@gmail.com>
2025-08-14 14:07:30 -0300

    adicionando teste de error

---
+++ tests/test_conditions_error.py
@@ -0,0 +1,35 @@
+from fastapi.testclient import TestClient
+from main import app
+import json
+
+client = TestClient(app)
+
+def test_create_contact():
+    new_contact = {
+        "name": "jonnathas",
+        "phone": "5511999999999"
+    }
+
+    response = client.post("/v1/contacts", json=new_contact)
+    assert response.status_code == 201
+
+def test_create_contact_error():
+    new_contact = {
+        "name": "jonnathas",
+        "phone": "5511999999999"
+    }
+
+    response = client.post("/v1/contacts", json=new_contact)
+    assert response.status_code == 400
+
+
+def test_delete_contact():
+    phone_contact = {"phone": "5511999999999"}
+    response = client.request(
+        method="DELETE",
+        url="/v1/contacts/delete-by-phone",
+        data=json.dumps(phone_contact),
+        headers={"Content-Type": "application/json"},
+    )
+    
+    assert response.status_code == 200
